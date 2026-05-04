0cafb3666b8f78b17f80ba317ae98d3af01cdd2a
JabinSultana <jabinasiff9926@gmail.com>
2026-05-04 16:48:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Posts from "./Posts";
+import Users from "./Users";
+import Notifications from "./Notifications";
+import PostDetail from "./PostDetail";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [posts, setPosts] = useState([
+    { id: 1, title: "First Post", content: "Hello world", user: "User1", reactions: [0,0,0,0,0] }
+  ]);
 
-export default App
+  return (
+    <div className="App">
+      {/* Do not remove the main div */}
+
+      <Router>
+        <nav>
+          <a href="/">Posts</a>
+          <a href="/users">Users</a>
+          <a href="/notifications">Notifications</a>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Posts posts={posts} setPosts={setPosts} />} />
+          <Route path="/users" element={<Users posts={posts} />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/posts/:id" element={<PostDetail posts={posts} setPosts={setPosts} />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
